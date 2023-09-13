d688105689245eba08aacaa2c525f7c4fa265f7f
Swapnil Devare <117857397+swapnildevare@users.noreply.github.com>
2023-09-13 17:40:08 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# Make variables
+name = "Swapnil"
+acc = "Github"
+work = "Programmer"
+this = "Tech"
+
+# You can say here anything 
+string = f"Hey how are you? my name is {name} and welcome to my {acc} profile here you can find some thing that gonna help you in \
+your coding journey. You think im a {work} but im not im just a {this} lover. Ok, here i stop boring you!"
+
+# Print the String           
+print(string)           
+            
